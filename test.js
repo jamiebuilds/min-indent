e8eac7f1ce8f6601f22cd5eda3d20d3d37df7764
Jamie Kyle <me@thejameskyle.com>
2020-05-28 12:15:10 -0700

    fixes

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-import test from 'ava';
-import m from '.';
+const test = require('ava');
+const m = require('.');
 
-test(t => {
+test('works', t => {
 	t.is(m('\nunicorn\n'), 0);
 	t.is(m('\n  unicorn\n'), 2);
 	t.is(m('\t\t<!doctype html>\n\t\t<html>\n\t\t\t<body>\n\n\n\n\t\t\t\t<h1>Hello world!</h1>\n\t\t\t</body>\n\t\t</html>'), 2);
